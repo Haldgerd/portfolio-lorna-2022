df9e46198108e7abad9957db6ac8ea73628f712b
Laura Lesjak <Laura.Lesjak@endava.com>
2022-04-05 13:14:57 +0200

    "Move files to portfolio folder."

--- portfolio-lorna/src/main.tsx
+++ portfolio-lorna/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/styles/index.module.css";
-import App from "./App.tsx";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
