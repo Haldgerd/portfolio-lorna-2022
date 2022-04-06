0a46f637d6d6c1571849079fd8d54a600f1c7a2a
Laura Lesjak <Laura.Lesjak@endava.com>
2022-04-06 08:41:41 +0200

    "Add svg icons, add image, fonts, modify eslint configuration, prettier configruation."

--- portfolio-lorna/src/App.tsx
+++ portfolio-lorna/src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./assets/styles/index.module.css";
+import { Icon } from "@iconify/react";
 
 function App() {
-  return <header className="App-header">I AM LORNA</header>;
+  return (
+    <>
+      <header className="text-3xl font-bold">I AM LORNA</header>
+      <Icon icon="mdi:robot-confused" />)
+    </>
+  );
 }
 
 export default App;
