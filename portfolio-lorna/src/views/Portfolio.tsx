908d3c6674c1fce9144301994dd9aba239dbb9be
Haldgerd <haldgerd.code@gmail.com>
2022-04-20 17:08:12 +0200

    "Modify and add additional config rules to ts.config file, modify package.json. Remove redundant css file."

--- portfolio-lorna/src/views/Portfolio.tsx
+++ portfolio-lorna/src/views/Portfolio.tsx
@@ -3,7 +3,6 @@ import Sidebar from "../components/Layout/SideBar/Sidebar";
 import NavigationArea from "../components/Navigation/NavigationArea";
 import Image from "../components/UI/Image";
 import Circle from "../components/Visuals/Circle/Circle";
-import styles from "./portfolio.module.css";
 
 const Portfolio: React.VFC = (): JSX.Element => {
     return (
@@ -11,9 +10,7 @@ const Portfolio: React.VFC = (): JSX.Element => {
             <Sidebar />
             <NavigationArea />
             <Image />
-            <Circle
-                circleClass={`${styles.circle} tw-absolute -z-10 tw-bg-red-500 tw-rounded-full -tw-top-17 tw-left-36 tw-w-70 tw-h-70`}
-            />
+            <Circle circleClass="tw-absolute -z-10 tw-bg-red-500 tw-rounded-full -tw-top-17 tw-left-36 tw-w-70 tw-h-70" />
         </section>
     );
 };
