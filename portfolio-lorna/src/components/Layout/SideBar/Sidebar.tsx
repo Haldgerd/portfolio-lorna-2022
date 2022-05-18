91a115960012466d2ed3b74f519f0c394541d065
Haldgerd <haldgerd.code@gmail.com>
2022-05-18 20:57:19 +0200

    "Fix typescript css module recognition, move favicons and manifest file into public folder."

--- portfolio-lorna/src/components/Layout/SideBar/Sidebar.tsx
+++ portfolio-lorna/src/components/Layout/SideBar/Sidebar.tsx
@@ -2,21 +2,19 @@ import React from "react";
 import year from "../../../util/helpers/generateDate";
 import styles from "./sidebar.module.css";
 
-const Sidebar: React.VFC = () => {
+const Sidebar: React.VFC = (): JSX.Element => {
     return (
         <aside className="tw-bg-yellow-400 tw-h-screen tw-w-12 tw-fixed tw-left-10 tw-z-10 tw-text-black tw-py-2 tw-flex tw-flex-col tw-justify-between tw-items-center">
             <div>
                 <p className={`${styles.sidebar__author} tw-m-auto tw-mb-7`}>
                     created with RAM by{" "}
                     <span
-                        className={`${styles["sidebar__circle--small"]} tw-bg-red-700 tw-relative`}
+                        className={`${styles["sidebar__circle--small"]} tw-bg-red-700`}
                     />
                     <span className="tw-text-teal-600">ローナ “LORNA”</span>
                 </p>
                 <time dateTime={String(year)}>
-                    <span
-                        className={`${styles.sidebar__square} tw-bg-red-700`}
-                    />
+                    <span className={`${styles.sidebar__square} tw-bg-black`} />
                     {year()}
                 </time>
             </div>
--- portfolio-lorna/src/views/About.tsx
+++ portfolio-lorna/src/views/About.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About: React.VFC = (): JSX.Element => {
-    return <section className="tw-h-4/5 tw-bg-red-500" />;
+    return <section className="tw-h-4 tw-bg-red-500" />;
 };
 
 export default About;
