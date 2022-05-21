68755200e1798288bc5c1ad506a2df774f0882f3
Laura "Lorna" Mali <59872477+Haldgerd@users.noreply.github.com>
2022-05-21 21:34:43 +0200

    Portfolio develop (#9)
    * refactor/portfolio: added navigation buttons to other pages, created About page template.
    
    * feat/portfolio: added About page with navigation and sidebar, re-organized components structure in Landing page.
    
    * feat/portfolio: added Header component template, added base styling.

---
+++ portfolio-lorna/src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import styles from './header.module.css';
+
+const Header: React.FC = (): JSX.Element => {
+    return (
+        <header className="tw-col-start-4 tw-col-end-8 tw-row-start-2">
+            <h1 className="tw-text-9xl">I AM LORNA</h1>
+            {/* <EdgeLine /> */}
+        </header>
+    );
+};
+
+export default Header;
