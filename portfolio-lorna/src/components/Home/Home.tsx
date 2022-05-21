edfcdb37ada72f1cad1d8d9708ad6bfc833a3d61
Haldgerd <haldgerd.code@gmail.com>
2022-05-21 21:33:58 +0200

    feat/portfolio: added Header component template, added base styling.

--- portfolio-lorna/src/components/Home/Home.tsx
+++ portfolio-lorna/src/components/Home/Home.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import Header from '../Header/Header';
 import Image from '../UI/Image';
 import Circle from '../Visuals/Circle/Circle';
 import styles from './home.module.css';
 
 const Home: React.VFC = (): JSX.Element => {
     return (
-        <section className="grid">
+        <section className="tw-grid tw-grid-cols-7 tw-grid-rows-4">
             <Image />
             <Circle
                 circleClass={`${styles.circle__position} tw-absolute -z-10 tw-bg-red-500 tw-rounded-full -tw-top-17 tw-left-36 tw-w-70 tw-h-70`}
             />
-            {/* <Header /> */}
+            <Header />
         </section>
     );
 };
